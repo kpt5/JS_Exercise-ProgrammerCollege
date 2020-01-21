75b61a9d66320313119f3a0e73ec83d7a04b8f3c
taisuke <kpt.555.b@gmail.com>
2020-01-21 18:05:35 +0900

    初回_200121_1805

---
+++ JS2.js
@@ -0,0 +1,15 @@
+//document.write("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+//document.getElementById("aGroup").innerHTML  ="aaaaaa";
+
+//document.getElementById("aGroup").innerText = "aaaaaa";
+
+//document.getElementById("aGroup").feeSum_r(1, 1);
+document.getElementById("aGroup").innerText = feeSum_r(2,4);
+document.getElementById("bGroup").innerText = feeSum_r(1,5);
+document.getElementById("cGroup").innerText = feeSum_r(3,7);
+//document.getElementById("cGroup").write(feeSum_r(3,7));
+
+//$(document).ready(function () {
+//  $("#cGroup").feeSum_r(3, 7);
+//
+//});
